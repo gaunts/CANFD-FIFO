e9342272b80594c6ff5738e8ae4a5795beacc30b
Simon Diradourian <simond@kickmaker.net>
2022-04-11 17:16:22 +0200

    cantests archi

--- linux/spamreader.cpp
+++ linux/spamreader.cpp
@@ -10,15 +10,15 @@ CppCan::CanController controller_;
 uint64_t count = 0;
 std::thread logthread;
 
-void logLoop()
-{
-    while (true)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        std::system("clear");
-        std::cout << count << std::endl;
-    }
-}
+// void logLoop()
+// {
+//     while (true)
+//     {
+//         std::this_thread::sleep_for(std::chrono::milliseconds(1));
+//         std::system("clear");
+//         std::cout << count << std::endl;
+//     }
+// }
 
 int main()
 {
@@ -31,19 +31,21 @@ int main()
                                 standardBitrate,
                                 extendedBitrate);
 
-    std::thread t(&logLoop);
+    // std::thread t(&logLoop);
     if (!connected)
     {
         std::cout << "Cannot connect" << std::endl;
         return 0;
     }
+    controller_.EnableIxaatFix();
 
     CppCan::CanMessage message;
     while (true)
     {
-
         if (!controller_.ReadMessage(message))
             continue;
+        // message.SetDataSize(8);
+        // std::cout << message.DataSize() << std::endl;
         controller_.SendMessage(message);
     }
 }
