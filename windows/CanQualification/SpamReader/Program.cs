bce89cbd98abe5ab4b3675543f3c63fca435a079
Simon Diradourian <simond@kickmaker.net>
2022-04-14 18:42:20 +0200

    canqualif update

--- windows/CanQualification/SpamReader/Program.cs
+++ windows/CanQualification/SpamReader/Program.cs
@@ -1,2 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+
+int track = 0;
+AutoResetEvent _receptionEvent = new AutoResetEvent(false);
+int sentMessages = 0;
+Stopwatch sw = new Stopwatch();
+CanManager.CanManager.Connect(1);
+
+Console.CursorVisible = false;
+//_ = SendLoop();
+sw.Start();
+
+_ = DisplayLoop();
+
+while (true)
+{
+    var msg = CanManager.CanManager.WaitForMessageReception();
+    track = msg[0];
+    CanManager.CanManager.SendMessage(track);
+    sentMessages++;
+}
+
+async Task DisplayLoop()
+{
+    await Task.Run(async () =>
+    {
+        while (true)
+        {
+            if (sentMessages == 0)
+            {
+                sw.Restart();
+            }
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine(sentMessages);
+            Console.Write((((double)sentMessages / (double)sw.ElapsedMilliseconds) * 1000).ToString("F"));
+            Console.WriteLine(" / s                ");
+            Console.WriteLine(track + "     ");
+            await Task.Delay(1);
+        }
+    });
+}
+
+//async Task SendLoop()
+//{
+//    while (true)
+//    {
+//        await Task.Run(() =>
+//        {
+//            CanManager.CanManager.SendMessage();
+//            CanManager.CanManager.WaitForMessageReception();
+//            sentMessages++;
+//        });
+//    }
+//}
