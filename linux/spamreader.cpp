6e384adf03121ad1540c891a564cd00e25367c74
mickael <mickael@kickmaker.net>
2022-04-14 18:43:17 +0200

    spamreader update

--- linux/spamreader.cpp
+++ linux/spamreader.cpp
@@ -8,6 +8,7 @@
 
 CppCan::CanController controller_;
 uint64_t count = 0;
+int lastReceivedTrack = -1;
 std::thread logthread;
 
 void SetCursorPosition(int XPos, int YPos) {
@@ -19,17 +20,21 @@ void logLoop()
     auto startTime = std::chrono::system_clock::now();
     while (true)
     {
+        if (count == 0)
+            startTime = std::chrono::system_clock::now();
         auto now = std::chrono::system_clock::now();
         SetCursorPosition(0, 0);
         std::cout << count << std::endl;
         double seconds = std::chrono::duration_cast<std::chrono::milliseconds>(now - startTime).count() / 1000.0;
         std::cout << (double)count / seconds << " / s                          " << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << lastReceivedTrack << "  " << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
 
 int main()
 {
+    system("clear");
     CppCan::CanBitrate standardBitrate(CppCan::eCanBittimingMode_Raw, 2, 63, 16, 16, 0);
     CppCan::CanBitrate extendedBitrate(CppCan::eCanBittimingMode_Raw, 2, 5, 4, 4, 0);
     bool connected = controller_.Open(CppCan::CanOperatingMode::eCanOperatingMode_Standard,
@@ -39,21 +44,24 @@ int main()
                                 standardBitrate,
                                 extendedBitrate);
 
-    std::thread t(&logLoop);
     if (!connected)
     {
         std::cout << "Cannot connect" << std::endl;
         return 0;
     }
+    std::thread t(&logLoop);
     // controller_.EnableIxaatFix();
 
     CppCan::CanMessage message;
-    int count = 0;
     while (true)
     {
         if (!controller_.ReadMessage(message))
             continue;
         if (message.DataSize() == 8 && message[2] == 0x22)
+        {
+            lastReceivedTrack = message[0];
             controller_.SendMessage(message);
+            count++;
+        }
     }
 }
